1cf1369d1b26028ef763d71439f22f758a5e0a2a
sidpd51 <sidpd510@gmail.com>
2024-09-27 00:24:31 +0530

    feat: initialize basic route setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 const { PORT } = require("./config/serverConfig");
+const apiRoutes = require('./routes/index')
+
 
 const setupAndStartServer = () => {
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/api',apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server started on port: ${PORT}`);
