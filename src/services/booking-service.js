c02781eeb738728da8e1e0033a54c3a60622f388
sidpd51 <sidpd510@gmail.com>
2024-10-01 00:13:43 +0530

    Enhance createBooking: Add seat validation and total cost calculation

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -12,9 +12,26 @@ class BookingService {
         try {
             const flightId = data.flightId;
             let getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
-            const flight = await axios.get(getFlightRequestURL);
-            return flight;
+            const response = await axios.get(getFlightRequestURL);
+            const flightData = response.data.data;
+            let priceOfTheFlight = flightData.price;
+            if (data.noOfSeats > flightData.totalSeats) {
+                throw new ServiceError(
+                    "Something went wrong in the booking process",
+                    "Insufficient seats in the flight"
+                );
+            }
+            const totalCost = priceOfTheFlight * data.noOfSeats;
+            const bookingPayload = { ...data, totalCost };
+            const booking = await this.BookingRepository.create(bookingPayload);
+            return booking;
         } catch (error) {
+            if (
+                error.name == "RepositoryError" ||
+                error.name == "ValidationError"
+            ) {
+                throw error;
+            }
             throw new ServiceError();
         }
     }
